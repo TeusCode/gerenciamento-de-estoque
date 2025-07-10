48321300081f47fa12bc7de519e85579fb265da3
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-07-10 10:08:07 -0300

    Melhoria no design

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById("menu-lateral")
+function abrirMenu() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
+}
