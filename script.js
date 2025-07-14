1438acf9a9fd39e59ebbde9c63cfcc00ad6e9b2b
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2025-07-14 16:16:02 -0300

    melhorando o menu

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const menu = document.getElementById("menu-lateral")
 
 function abrirMenu() {
-    if (menu.style.display === "none") {
-        menu.style.display = "block"
-
-    } else {
-        menu.style.display = "none"
-
-    }
+    menu.style.left = "0px";
 }
