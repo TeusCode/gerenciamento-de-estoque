7fce69fb4d383c5639e270be1111eb5f7b425ee1
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2025-07-15 15:42:42 -0300

    Criando formulários de entrada saída e devolução

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ menu.onclick = e => {
     if (!conteiner.classList.contains('oculto')) {
         conteiner.classList.add('oculto')
         main.style.marginLeft = "63px"
-        main.style.width = "350px"
+        main.style.width = "calc(100vw - 107px)"
     } else {
         conteiner.classList.remove('oculto')
         main.style.marginLeft = "260px"
-        main.style.width = "150px"
+        main.style.width = "calc(100vw - 304px)"
     }
 }
